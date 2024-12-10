c7272990bfd52cde1940b61c217112f682638ddc
Moe-hacker <moe-hacker@outlook.com>
2024-12-10 07:27:19 +0000

    Double umount before remove container

--- stop.sh
+++ stop.sh
@@ -6,6 +6,11 @@ export PATH="$MODDIR/bin:$PATH"
 ruriumount() {
     fuser -k "$CONTAINER_DIR"
     ruri -U "$CONTAINER_DIR"
+    umount -lvf "$CONTAINER_DIR"
+    umount -lf "$CONTAINER_DIR"/sdcard
+    umount -lf "$CONTAINER_DIR"/sys
+    umount -lf "$CONTAINER_DIR"/proc
+    umount -lf "$CONTAINER_DIR"/dev
 }
 
 ruriumount
